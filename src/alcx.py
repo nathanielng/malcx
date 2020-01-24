545d2b60726cb085a26939ec6107a88606bce5ce
Nathaniel Ng <nathanielng@users.noreply.github.com>
2020-01-24 13:28:45 +0800

    Add run_job, evaluation_function

--- src/alcx.py
+++ src/alcx.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python
 
 import datetime
+import hyperopt
 import json
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
+import re
+import subprocess
 
 
 def load_json(filename):
@@ -16,7 +19,7 @@ def load_json(filename):
         return json.load(f)
 
 
-def print_dict(d, indent=1, dest="stdout"):
+def print_json(d, indent=1, dest="stdout"):
     """
     Pretty prints a Python dictionary
     to stdout or a filename
@@ -51,6 +54,36 @@ def dict_to_dataframe(d):
     tuple_list, data = dict_to_tuplelist(d)
     idx = pd.MultiIndex.from_tuples(tuple_list)
     return pd.DataFrame([data], columns=idx)
+
+
+def run_job(path):
+    process = subprocess.Popen(
+        [path],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+    stdout, stderr = process.communicate()
+    return stdout.decode(), stderr.decode()
+
+
+def get_coefficient_of_determination(txt):
+    m = re.search(r'Coefficient of determination\s*([0-9.]+)\s*±\s*([0-9.]+)', txt)
+    if m is None:
+        return None
+    else:
+        return {
+            'coeff': float(m.group(1)),
+            'uncertainty': float(m.group(2))
+        }
+
+
+def evalution_function(params, json_file, exec_path):
+    write_dict(params, json_file)
+    stdout, stderr = run_job(exec_path)
+    result = get_coefficient_of_determination(stdout)
+    if result is not None:
+        return result['coeff']
+    else:
+        return None
 
 
 if __name__ == "__main__":
