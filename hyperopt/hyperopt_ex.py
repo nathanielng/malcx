082929fe0b7c1bfabb89e05335c58e5bc56fcafa
Nathaniel Ng <nathanielng@users.noreply.github.com>
2020-01-30 18:10:03 +0800

    Add dataframes and better output

--- hyperopt/hyperopt_ex.py
+++ hyperopt/hyperopt_ex.py
@@ -3,6 +3,7 @@
 import hyperopt
 import matplotlib.pyplot as plt
 import numpy as np
+import pandas as pd
 
 
 def fn_to_optimize(x):
@@ -24,16 +25,31 @@ def plot_fn(x, y, filename):
     plt.savefig(filename)
 
 
-def hyperopt_optimize(my_fn, x_min, x_max):
+def hyperopt_optimize(my_fn, x_min, x_max,
+                      algo=hyperopt.rand.suggest):
+    trials = hyperopt.Trials()
     result = hyperopt.fmin(
         fn=my_fn,
         space=hyperopt.hp.uniform('x', x_min, x_max),
-        algo=hyperopt.rand.suggest,
-        trials=hyperopt.Trials(),
+        algo=algo,
+        trials=trials,
         max_evals=100,
         rstate=np.random.RandomState(100)
     )
-    return result
+    return {
+        'result': result,
+        'trials': trials
+    }
+
+
+def trials2df(result, trials):
+    x = pd.Series(trials.idxs_vals[1]['x'])
+    loss = pd.Series([x['loss'] for x in trials.results])
+    df = pd.DataFrame({
+        'x': x,
+        'loss': loss
+        }, index = trials.idxs_vals[0]['x'])
+    return df
 
 
 if __name__ == "__main__":
@@ -41,4 +57,11 @@ if __name__ == "__main__":
     y = fn_to_optimize(x)
     plot_fn(x, y, 'hyperopt.png')
     result = hyperopt_optimize(fn_to_optimize, x.min(), x.max())
-    print(f"Result = {result}")
+    print(f"Result = {result['result']}")
+    df = trials2df(result['result'], result['trials'])
+    print(df.head())
+    print(df.tail())
+    print()
+    idxmin = df['loss'].idxmin()
+    print(f"Best x: {df.loc[idxmin, 'x']}")
+    print(f"with loss = {df['loss'].min()} (at iteration # {idxmin})")
