dcfa1b180ca5d931c0e444d20a8f7da55c3f68ca
Nathaniel Ng <nathanielng@users.noreply.github.com>
2020-03-05 18:04:28 +0800

    Add correlation & scatter matrix plots
    Additional changes:
    - add refresh option to regenerate plots,
    overwriting previous plots
    - add subroutine to sort parameter history files by time stamps
    - bug fix: best coefficient of determination is the maximum one,
      not the minimum one

--- src/alcx_plot.py
+++ src/alcx_plot.py
@@ -4,12 +4,15 @@
 # Postprocessing subroutines
 
 import argparse
+import datetime
 import glob
 import matplotlib.pyplot as plt
 import os
 import pandas as pd
 import re
 
+from matplotlib.cm import get_cmap
+from pandas.plotting import scatter_matrix
 
 RUN_FOLDER = os.path.abspath('../../run/')
 
@@ -39,6 +42,16 @@ def print_params(s):
         print(f'       {param}: {value}')
 
 
+def add_timestamps(filenames, fmt=r'parameter_history_.*([0-9]{8}-[0-9]{4})h.csv'):
+    file_list = []
+    for filename in filenames:
+        m = re.search(fmt, filename)
+        if m is not None:
+            ts = datetime.datetime.strptime(m.group(1), '%Y%m%d-%H%M')
+            file_list.append([filename, ts])
+    return pd.DataFrame(file_list, columns=['Filename', 'Date']).sort_values('Date')
+
+
 def summarize_historyfile(filename):
     """
     Summarizes a parameter history file
@@ -54,13 +67,13 @@ def summarize_historyfile(filename):
         print(f'Columns: {df.columns}')
 
     col = 'coefficient of determination'
-    best_coeff = df[col].min()
-    idxmin = df[col].idxmin()
+    best_coeff = df[col].max()
+    idxmax = df[col].idxmax()
 
     print(f'(n={len(df)})')
-    print(f"   - Best coefficient of determination = {best_coeff} (at idx={idxmin})")
+    print(f"   - Best coefficient of determination = {best_coeff} (at idx={idxmax})")
     print(f"   - Best set of parameters:")
-    print_params(df.loc[idxmin, :].T)
+    print_params(df.loc[idxmax, :].T)
     return df
 
 
@@ -71,16 +84,45 @@ def plot_parameter_histograms(df, filename=None):
 
 
 def plot_parameter_history(df, filename=None):
-    if len(df.columns) != 12:
-        columns = ','.join(df.columns)
-        print('Skipping because columns != 12: {columns}')
-        return
     df.plot.line(marker='o', linestyle='', figsize=(12,12), layout=(4, 3), subplots=True)
     if filename is not None:
         plt.savefig(filename)
 
 
-def load_history_files(folder):
+def plot_parameter_correlation(df, filename=None):
+    n_rows, n_cols = 4, 3
+    colors = get_cmap('Paired').colors
+    cod_idx = df.columns.get_loc('coefficient of determination')
+    fig, axs = plt.subplots(n_rows, n_cols, figsize=(15, 12))
+    i = 0
+    for row in range(n_rows):
+        for col in range(n_cols):
+            axs[row, col].plot(
+                df.iloc[:, i],
+                df.iloc[:, cod_idx],
+                'o',
+                color=colors[i],
+                label=df.columns[i])
+            axs[row, col].legend(frameon=False)
+            i += 1
+    if filename is not None:
+        plt.savefig(filename)
+
+
+def plot_parameter_correlation2(df, filename=None):
+    df_new = df.set_index('coefficient of determination')
+    df_new.plot.line(marker='o', linestyle='', figsize=(12,12), layout=(4, 3), subplots=True)
+    if filename is not None:
+        plt.savefig(filename)
+
+
+def plot_scatter_matrix(df, filename=None):
+    scatter_matrix(df, figsize=(20,20));
+    if filename is not None:
+        plt.savefig(filename)
+
+
+def load_history_files(folder, refresh):
     print(f'Run Folder: {folder}')
     files = glob.glob(f'{folder}/parameter_history*.csv')
     for i, file in enumerate(files):
@@ -91,7 +133,7 @@ def load_history_files(folder):
         print(f'{i}: {basename} ', end='')
         df = summarize_historyfile(file)
 
-        if os.path.isfile(img_file1):
+        if refresh is False and os.path.isfile(img_file1):
             continue
         plot_parameter_histograms(df, img_file1)
         plot_parameter_history(df, img_file2)
@@ -100,9 +142,10 @@ def load_history_files(folder):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument('--history', default=None, help='History folder for parameters')
+    parser.add_argument('--refresh', action='store_true', help='Force image refresh')
     args = parser.parse_args()
 
     if args.history is not None:
-        load_history_files(args.history)
+        load_history_files(args.history, args.refresh)
     else:
-        load_history_files(RUN_FOLDER)
+        load_history_files(RUN_FOLDER, args.refresh)
